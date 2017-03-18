2c1324a128a6e8789b65bace7adcf176ce06ae81
michalpawlikowski <michal@strona.pl>
2017-03-18 13:58:16 +0100

    dodanie struktury danych 20170318

---
+++ struktury-danych-przetwarzanie/data.js
@@ -0,0 +1,64 @@
+var data = {
+  students: [
+    {
+      id: 1,
+      name: 'Jacek',
+      notes: [5, 3, 4, 2, 5, 5]
+    },
+    {
+      id: 2,
+      name: 'Ewa',
+      notes: [2, 3, 3, 3, 2, 5]
+    },
+    {
+      id: 3,
+      name: 'Zygmunt',
+      notes: [2, 2, 4, 4, 3, 3]
+    }
+  ],
+  groups: [
+    {
+      id: 1,
+      name: 'JFDD-1',
+      students: [1, 2]
+    },
+    {
+      id: 2,
+      name: 'JFDD-2',
+      students: [1, 3]
+    },
+    {
+      id: 3,
+      name: 'JFDD-3',
+      students: [3]
+    }
+  ],
+  favoriteTracks: [
+    {
+      id: 1,
+      studentId: 2,
+      tracks: ['A', 'B', 'C']
+    },
+    {
+      id: 2,
+      studentId: 1,
+      tracks: ['A', 'B']
+    },
+    {
+      id: 3,
+      studentId: 3,
+      tracks: ['C']
+    }
+  ]
+};
+
+var studentci = [
+    {
+      name: 'Jacek',
+      isSmoking: true
+    },
+    {
+      name: 'Janusz',
+      isSmoking: false
+    }
+  ]
